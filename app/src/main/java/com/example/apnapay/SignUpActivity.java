0931bba07fa1c3f4e957246fb4fe269d6ba617e6
wasee-01 <124555385+Wasee-Ur-Rehman@users.noreply.github.com>
2026-05-04 02:50:18 +0500

    feat(auth): add signup screen

---
+++ app/src/main/java/com/example/apnapay/SignUpActivity.java
@@ -0,0 +1,23 @@
+package com.example.apnapay;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_sign_up);
+
+        ImageView btnBack = findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(v -> getOnBackPressedDispatcher().onBackPressed());
+    }
+}
