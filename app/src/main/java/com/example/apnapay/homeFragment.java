79f36e23d85b43726928de3c5a486d36aaaa074e
wasee-01 <124555385+Wasee-Ur-Rehman@users.noreply.github.com>
2026-05-04 18:12:48 +0500

    feat(home): add home screen with recycler view

---
+++ app/src/main/java/com/example/apnapay/homeFragment.java
@@ -0,0 +1,31 @@
+package com.example.apnapay;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class homeFragment extends Fragment {
+
+
+    public homeFragment() {
+        // Required empty public constructor
+    }
+    public static homeFragment newInstance(String param1, String param2) {
+        homeFragment fragment = new homeFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
